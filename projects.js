31c9b94a08138857a65668f2e99c39a6892a24d7
Bob Cao <bobcaocheng@gmail.com>
2019-10-21 16:45:08 -0700

    Setting up backend APIs and front end

--- projects.js
+++ projects.js
@@ -1,6 +1,7 @@
 const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
 const Project = require('./project.js')
+const uuidv4 = require('uuid/v4')
 
 let Pm = class {
     constructor(projDBFile) {
@@ -8,28 +9,17 @@ let Pm = class {
         this.adapter = new FileSync('db.json')
         this.db = low(this.adapter)
 
-        this.db.defaults({ projects: [] }).write()
+        this.db.defaults({ projects: {} }).write()
     }
 
     createProject(proj) {
-        this.db.get('projects')
-               .push(proj)
-               .write()
+        let uuid = uuidv4()
+        this.db.get('projects').set(uuid, proj).write()
+        return uuid
     }
 
     getProjects() {
         return this.db.get('projects')
-    }
-
-    createProjectHandler(request) {
-        console.log("Received POST from " + request.payload.name + "; id=" + (request.payload.id || 'anon'));
-
-        if (request.payload.uploadFile) {
-            const byteLength = Buffer.byteLength(request.payload.uploadFile);
-            console.log('Persist Buffer here. Size=' + byteLength);
-        }
-
-        return {greeting: 'POST hello to ' + request.payload.name}
     }
 
     createRoutes(server) {
@@ -44,7 +34,21 @@ let Pm = class {
         server.route({
             method: 'POST',
             path: '/api/createProject',
-            handler: this.createProjectHandler
+            handler: (request) => {
+                let payload = request.payload
+
+                if (payload) {
+                    if (!payload.projName) {
+                        return { status: "failed", error: "Request doesn't contain projName field" }
+                    }
+                    let proj = new Project(payload.projName)
+                    let uuid = this.createProject(proj)
+
+                    return { status: "success", objectUUID: uuid }
+                }
+
+                return { status: "failed", error: "Empty payload" }
+            }
         })
     }
 }
--- server.js
+++ server.js
@@ -2,22 +2,41 @@
 
 const Hapi = require('@hapi/hapi')
 const Pm = require('./projects.js')
+const Path = require('path');
 
 let pm = new Pm("projectsDB.json");
 
 const init = async () => {
 
     const server = Hapi.server({
-        port: 3000,
-        host: 'localhost'
+        port: 1337,
+        host: 'localhost',
+        routes: {
+            files: {
+                relativeTo: Path.join(__dirname, 'public')
+            }
+        }
     });
+
+    await server.register(require('@hapi/inert'));
 
     server.route({
         method: 'GET',
         path: '/',
         handler: (request, h) => {
-            return 'Hello World!'
+            return h.file('app.html')
         },
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/{param*}',
+        handler: {
+            directory: {
+                path: '.',
+                redirectToSlash: true
+            }
+        }
     });
 
     pm.createRoutes(server)
